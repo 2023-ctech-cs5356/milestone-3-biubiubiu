0c871e54dc7a88309595fa4dccd265332a46f831
YseraHuang <zh432@cornell.edu>
2023-04-29 16:21:16 -0400

    Finish Initial version of Database

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,7 +6,6 @@ import firebase from 'firebase/compat/app'
 // This imports the Firebase Auth libraries
 import 'firebase/compat/auth'
 
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBZAu21Xnht97pEAqxmNf1CyJHkHFjxudI",
@@ -30,7 +29,29 @@ const uiConfig = {
   },
 };
 
+
+
 function Home() {
+
+  const handleSubmit = e => {
+    console.log(firebase.auth().currentUser.uid)
+    e.preventDefault()
+    fetch('/api/order',{
+      method:"POST",
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({
+        from:e.target.from.value, 
+        to:e.target.to.value,
+        max:e.target.max.value,
+        ownerId: firebase.auth().currentUser.uid})
+    }).then(response=>{
+      if (response.ok){
+        console.log("Add Order Ok")
+      }
+    })
+  }
 
   const [isSignedIn, setIsSignedIn] = useState(false);
   
@@ -57,6 +78,16 @@ function Home() {
     <div>
       <h1>My App</h1>
       <p>Welcome - You are now signed-in - {isSignedIn}!</p>
+      <form onSubmit={handleSubmit}>
+        <div>from</div>
+        <input name="from"></input>
+        <div>To</div>
+        <input name="to"></input>
+        <div>Max</div>
+        <input name="max"></input>
+        <input type="submit"/>
+      </form>
+      <br></br>
       <button class="is-danger" onClick={()=>firebase.auth().signOut()}>sign out</button>
     </div>
   );
