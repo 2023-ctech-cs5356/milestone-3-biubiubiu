2dc5540d2728034bbff1e81c524be3bcf804056f
Jiarong Chen <37850306+JRRRRRRR@users.noreply.github.com>
2023-05-02 22:09:51 -0400

    frontend style

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 // This uses the pre-built login form
-import { StyledFirebaseAuth } from 'react-firebaseui';
+import { StyledFirebaseAuth } from "react-firebaseui";
 // This imports firebase using the Firebase SDK v8 style
-import firebase from 'firebase/compat/app'
+import firebase from "firebase/compat/app";
 // This imports the Firebase Auth libraries
-import 'firebase/compat/auth'
+import "firebase/compat/auth";
+
+import "./Home.css";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,84 +15,142 @@ const firebaseConfig = {
   projectId: "cs5356-milestone2-2b364",
   storageBucket: "cs5356-milestone2-2b364.appspot.com",
   messagingSenderId: "893513623486",
-  appId: "1:893513623486:web:e869fa4112a7b1e1c5bbef"
+  appId: "1:893513623486:web:e869fa4112a7b1e1c5bbef",
 };
 
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
 const uiConfig = {
-  signInFlow: 'popup',
-  signInOptions: [
-    firebase.auth.EmailAuthProvider.PROVIDER_ID
-  ],
+  signInFlow: "popup",
+  signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
   callbacks: {
     // Avoid redirects after sign-in.
     signInSuccessWithAuthResult: () => false,
   },
 };
 
-
-
 function Home() {
-
-  const handleSubmit = e => {
-    console.log(firebase.auth().currentUser.uid,firebase.auth().currentUser.displayName)
-    e.preventDefault()
-    fetch('/api/order',{
-      method:"POST",
-      headers:{
-        'Content-Type':'application/json'
+  const handleSubmit = (e) => {
+    console.log(
+      firebase.auth().currentUser.uid,
+      firebase.auth().currentUser.displayName
+    );
+    e.preventDefault();
+    fetch("/api/order", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-      body:JSON.stringify({
-        from:e.target.from.value, 
-        to:e.target.to.value,
-        max:e.target.max.value,
+      body: JSON.stringify({
+        from: e.target.from.value,
+        to: e.target.to.value,
+        max: e.target.max.value,
         ownerId: firebase.auth().currentUser.uid,
-        owner: firebase.auth().currentUser.displayName
+        owner: firebase.auth().currentUser.displayName,
       }),
-    }).then(response=>{
-      if (response.ok){
-        console.log("Add Order Ok")
+    }).then((response) => {
+      if (response.ok) {
+        console.log("Add Order Ok");
       }
-    })
-  }
+    });
+  };
 
   const [isSignedIn, setIsSignedIn] = useState(false);
-  
 
   useEffect(() => {
-    const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
-      // this gets called whenever a user signs in or out
-      setIsSignedIn(!!user); //!! convert user to boolean
-    });
+    const unregisterAuthObserver = firebase
+      .auth()
+      .onAuthStateChanged((user) => {
+        // this gets called whenever a user signs in or out
+        setIsSignedIn(!!user); //!! convert user to boolean
+      });
     return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
   }, []);
-
 
   if (!isSignedIn) {
     return (
       <div>
         <h1>My App</h1>
         <p>Please sign-in:</p>
-        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+        <StyledFirebaseAuth
+          uiConfig={uiConfig}
+          firebaseAuth={firebase.auth()}
+        />
       </div>
     );
   }
   return (
-    <div>
-      <h1>My App</h1>
-      <p>Welcome - You are now signed-in - {isSignedIn}!</p>
-      <form onSubmit={handleSubmit}>
-        <div>from</div>
-        <input name="from"></input>
-        <div>To</div>
-        <input name="to"></input>
-        <div>Max</div>
-        <input name="max"></input>
-        <input type="submit"/>
-      </form>
-      <br></br>
-      <button class="is-danger" onClick={()=>firebase.auth().signOut()}>sign out</button>
+    // <div>
+    //   <h1>My App</h1>
+    //   <p>Welcome - You are now signed-in - {isSignedIn}!</p>
+    //   <form onSubmit={handleSubmit}>
+    //     <div>from</div>
+    //     <input name="from"></input>
+    //     <div>To</div>
+    //     <input name="to"></input>
+    //     <div>Max</div>
+    //     <input name="max"></input>
+    //     <input type="submit"/>
+    //   </form>
+    //   <br></br>
+    //   <button class="is-danger" onClick={()=>firebase.auth().signOut()}>sign out</button>
+    // </div>
+    <div className="Home">
+      <nav>
+        <span>Welcome {firebase.auth().currentUser.displayName} !</span>
+        <div>
+          <button>My Order</button>
+          <button onClick={() => firebase.auth().signOut()}>Log out</button>
+        </div>
+      </nav>
+
+      <header>
+        <div className="wrap">
+          <form onSubmit={handleSubmit}>
+            <div>
+              <span>From:</span>
+              <input name="from"></input>
+            </div>
+            <div>
+              <span>To:</span>
+              <input name="to"></input>
+            </div>
+            <div>
+              <span>Max Passenger:</span>
+              <input name="max"></input>
+            </div>
+            <input
+              className="submitBtn button"
+              type="submit"
+              value={"Create A TrIp"}
+            ></input>
+          </form>
+        </div>
+        <div className="wrap">
+          <span className="titleName">Search by :</span>
+          <form>
+            <div>
+              <span>From:</span>
+              <input name="from"></input>
+            </div>
+            <div>
+              <span>To:</span>
+              <input name="to"></input>
+            </div>
+            <div>
+              <span>Max Passenger:</span>
+              <input name="max"></input>
+            </div>
+            <input
+              className="submitBtn button"
+              type="submit"
+              value={"Search"}
+            ></input>
+          </form>
+        </div>
+      </header>
+
+      <main></main>
     </div>
   );
 }
